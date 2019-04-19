7fd033cdffaef22e8ee12de0409c7b5b793ebb02
rj33536 <rj33536@gmail.com>
2019-04-19 10:42:02 +0530

    remove debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
